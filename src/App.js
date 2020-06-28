21ec169cce043ad3ed68e5cd5b9a9e9ae37905c2
Driijko <driijko@gmail.com>
2020-06-28 11:45:03 -0400

    Add functionality for deleting a cafe

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
   useEffect(()=> {
     if (cafes) {
       setCafeComponents(cafes.map(cafe=> {
-        return <Cafe cafeData={{name: cafe.name, city: cafe.city}} key={cafe.id} />
+        return <Cafe cafeData={{name: cafe.name, city: cafe.city, id: cafe.id}} key={cafe.id} handleClick={handleClick}/>
      }))
     }
   }, [cafes])
@@ -40,11 +40,17 @@ function App() {
 
   function handleClick(e) {
     e.preventDefault();
-    console.log(newCafeName, newCafeCity);
-    firebase.firestore().collection("cafes").add({
-      name: newCafeName,
-      city: newCafeCity
-    })
+    if (e.target.id === "addCafeButton") {
+      firebase.firestore().collection("cafes").add({
+        name: newCafeName,
+        city: newCafeCity
+      })
+    }
+    if (e.target.id === "deleteButton") {
+      let id = e.target.parentElement.id;
+      console.log(id);
+      firebase.firestore().collection("cafes").doc(id).delete();
+    }
   }
 
 
@@ -57,7 +63,7 @@ function App() {
         <form id="add-cafe-form">
           <input type="text" name="name" placeholder="Cafe Name" onChange={handleChange} />
           <input type="text" name="city" placeholder="Cafe City" onChange={handleChange} />
-          <button onClick={handleClick}>Add Cafe</button>
+          <button id="addCafeButton" onClick={handleClick}>Add Cafe</button>
         </form>
 
         <ul id="cafe-list">
--- src/components/Cafe.js
+++ src/components/Cafe.js
@@ -2,9 +2,11 @@ import React from "react";
 
 export default function Cafe(props) {
   return (
-    <li>
+    <li id={props.cafeData.id}>
       <span>{props.cafeData.name}</span>
       <span>{props.cafeData.city}</span>
+      <div id="deleteButton" onClick={props.handleClick}>x</div>
+      <div id="editButton">edit</div>
     </li>
   )
 }
